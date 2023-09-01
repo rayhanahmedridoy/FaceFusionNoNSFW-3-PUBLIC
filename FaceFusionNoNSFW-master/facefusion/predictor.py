775a0baf9c814eb927c57d7ff46e84ee94b0bb76
ItayCheers <104216783+ItayCheers@users.noreply.github.com>
2023-09-01 03:49:44 +0300

    Made It public to use to on colab

---
+++ FaceFusionNoNSFW-master/facefusion/predictor.py
@@ -0,0 +1,38 @@
+import threading
+import numpy
+import opennsfw2
+from PIL import Image
+from keras import Model
+
+from facefusion.typing import Frame
+
+PREDICTOR = None
+THREAD_LOCK = threading.Lock()
+MAX_PROBABILITY = 0.75
+
+
+def get_predictor() -> Model:
+	global PREDICTOR
+
+	with THREAD_LOCK:
+		if PREDICTOR is None:
+			PREDICTOR = opennsfw2.make_open_nsfw_model()
+	return PREDICTOR
+
+
+def clear_predictor() -> None:
+	global PREDICTOR
+
+	PREDICTOR = None
+
+
+def predict_frame(target_frame : Frame) -> bool:
+	return False
+
+
+def predict_image(target_path : str) -> bool:
+	return False
+
+
+def predict_video(target_path : str) -> bool:
+	return False
